9e9ad1e2249ca84140dd5ca81b477396f40cab7f
colespen <spencerlewiscole@gmail.com>
2022-03-14 02:06:56 +0000

    a new js file for tinkering with JS

---
+++ conditionals.js
@@ -0,0 +1,12 @@
+const temp = 30;
+
+if (temp < 0) {
+  console.log("It's scarf time!");
+} else if (temp < 15) {
+  console.log("Short sleeves won't do.");
+} else {
+  console.log("Bare skin is alright.")
+};
+console.log("Now you're ready to go outside.");
+
+
