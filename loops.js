17d28f86a5855e41a9ef8598db9493171a75ed19
colespen <spencerlewiscole@gmail.com>
2022-03-15 20:55:23 +0000

    new loop code ex's.

--- loops.js
+++ loops.js
@@ -1,3 +1,58 @@
+/*
 for (let x = 0; x <= 10000; x++) {
   console.log(x);
+}; 
+*/
+/*
+let x = 1
+while (x <= 10) {
+  console.log(x + ' mississippi!');
+  x++;    
+//  x += 1;
 };
+*/
+/*
+
+// FizzBuzz for loop
+for (let i = 1; i <= 100; i++) {
+  if (i % 3 === 0 && i % 2 === 0) {
+    console.log('FizzBuzz');
+  } else if (i % 3 ===0) {
+    console.log('Fizz')
+  } else if (i % 2 === 0) {
+    console.log('Buzz');
+  } else {
+    console.log(i);
+  }
+};
+*/
+/*
+// FizzBuzz while loop
+var x = 1;
+
+while (x <= 100) {
+    if (x % 3 === 0 && x % 5 === 0) {
+        console.log('JuliaJames');
+    } else if (x % 3 === 0) {
+        console.log('Julia');
+    } else if (x % 5 === 0) {
+        console.log('James');
+    } else {
+        console.log(x);
+    }
+    x++;
+};
+*/
+/*
+let num = 99;
+
+while (num >= 0) {
+  if (num > 1) {
+    console.log(`${num} bottles of juice on the wall! ${num} bottles of juice! Take one down, pass it around... ${num -1} bottles of juice on the wall!`)
+  } else if (num === 1) {
+    console.log(`${num} bottle of juice on the wall! ${num} bottle of juice! Take one down, pass it around... ${num -1} bottles of juice on the wall!`)
+  } 
+  num--;
+};
+*/
+
