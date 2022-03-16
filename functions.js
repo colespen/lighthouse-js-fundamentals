c20b33f33dd3e5ae74e11d3359aaf21ecb35cb9a
colespen <spencerlewiscole@gmail.com>
2022-03-16 03:33:34 +0000

    tinkering =>s

--- functions.js
+++ functions.js
@@ -1,7 +1,52 @@
+/*
 function revString(revMe) {
   let reversed = "";
-  for (let revMe of reversed)
+  for (let i = revMe.length -1; i >= 0; i--) {
+    reversed += revMe[i];
+  }
   return reversed;
 }
 console.log(revString("FatSlabb"));
+*/
 
+/*
+function reheatPizza(numSlices) {
+  // code that figures out reheat settings
+}
+*/
+
+/*
+function sayHi() {
+  var msg = "Yo sup dingus."
+  console.log(msg);
+}
+sayHi() //calls function, runs code
+*/
+
+/* function to find average number with open # of 
+arguments */
+/*
+
+let num = 0;
+let total = 0;
+const avg = function() {
+  for (let i of arguments) {
+  total += i; 
+  }
+  total /= num;
+  console.log(Math.round(total * 10) / 10);
+}
+
+num = 7;
+avg(67, 36, 47, 67, 4, 35, 700);
+*/
+
+/* //basic declaration
+
+function add(x,y) {
+  let sum = x + y;
+  return sum;
+}
+let sum = add(4, 20);
+console.log(sum);
+*/
