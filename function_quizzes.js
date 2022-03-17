4f3c55959418a4093b82016ea55d83d643d895e2
colespen <spencerlewiscole@gmail.com>
2022-03-17 19:16:02 +0000

    func quizzes in Udacity

---
+++ function_quizzes.js
@@ -0,0 +1,45 @@
+// Quiz Laugh
+/*
+let laugh = num => {
+  let sound = "";
+  for (let i = 1; i <= num; i++) {
+    sound += "ha";
+  }
+  sound += "!"
+  return sound;
+}
+*/
+// console.log(laugh(10));
+
+
+
+// Quiz Cry
+/*
+let cry = function boohoo() {
+  console.log("boohoo!");
+}
+cry();
+*//*
+let cry = function boohoo () {
+  let sound = "boohoo!"
+  return sound;
+}
+console.log(cry());
+*/
+
+
+// Quiz Inline
+/*
+function emotions(myString, myFunc) {
+  console.log("I am " + myString + ", " + myFunc(10));
+}
+emotions("happy", laugh = num => {
+  let sound = "";
+  for (let i = 1; i <= num; i++) {
+    sound += "ha";
+  }
+  sound += "!"
+  return sound;
+});
+*/
+
