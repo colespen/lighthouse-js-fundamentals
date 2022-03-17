6b6811be26de15a1917c878d7d3039649583af00
colespen <spencerlewiscole@gmail.com>
2022-03-17 19:52:42 +0000

    simple func expr

--- hello.js
+++ hello.js
@@ -1,8 +1,29 @@
+/* //func expr.
+
 const sayHello = function (name) {
     console.log("Hello, " + name);
 }
 sayHello("Poopie.");
 sayHello("Drinksle.");
 sayHello("Ringus.");
+*/
+
+/* //console.log:
+
+const sayHelloToConsole  = function (name) {
+  console.log("Hello, " + name);
+}
+sayHelloToConsole('John'); 
+*/
+
+/* //return:
+
+const returnSayHello  = function (name) {
+  return "Hello, " + name;
+}
+const greeting = returnSayHello('John');
+console.log(greeting);
+*/
+
 
 
