0b846dd14fdb469590b3382572fbef99d81a16ea
colespen <spencerlewiscole@gmail.com>
2022-03-17 19:16:59 +0000

    say Hello function

---
+++ functions2.js
@@ -0,0 +1,36 @@
+/* 
+//scope
+let one = 1;
+console.log(one);
+
+function diffOne() {
+  let one = 2;
+  console.log(one);
+}
+diffOne();
+console.log(one);
+*/
+
+ //Quiz 1
+
+ /* //          ***STUDY THIS ONE***
+
+ function makeLine(length) {
+  let line = "";
+  for (let j = 1; j <= length; j++) {
+    line += "* ";
+  }
+  return line + "\n";
+}
+
+function buildTriangle(x) {
+  let tri = "";
+  for (let i = 1; i <= x; i++) {
+    tri += makeLine(i);
+  }
+  return tri;
+}
+
+console.log(buildTriangle(10));
+*/
+
